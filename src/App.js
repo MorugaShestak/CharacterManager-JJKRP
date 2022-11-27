10683d346a9fca33140afb873499861d676bfdfc
Alexander Shestak <110448322+MorugaShestak@users.noreply.github.com>
2022-11-27 19:17:02 +0400

    AddCharacterButton template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import UpdateButton from "./components/ui/UpdateButton/UpdateButton";
-import AdminButton from "./components/ui/AdminButton/AdminButton";
+import AdminButton from "./components/ui/AddCharacterButton/AddCharacterButton";
 import Table from "./components/ui/Table/Table";
 import {useGetCharacters} from "./components/hooks/useGetCharacters";
+import AddCharacterButton from "./components/ui/AddCharacterButton/AddCharacterButton";
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
     <div className="App">
         <div className="Menu">
             <UpdateButton/>
-            <AdminButton/>
+            <AddCharacterButton/>
         </div>
 
         <div className="Table">
---
+++ src/components/ui/AddCharacterButton/AddCharacterButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCharacterButton = () => {
+    return (
+        <input className="btn btn-success" type="button" value="Add Character">
+
+        </input>
+    );
+};
+
+export default AddCharacterButton;
